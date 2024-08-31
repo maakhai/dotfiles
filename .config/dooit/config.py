00f8f0d5ba0f97fcc49ed5fccad639797a070c1b
Lucas Castro <lucaas.castro@outlook.com.br>
2024-08-31 14:15:53 -0300

    added dooit gruvbox theme

---
+++ .config/dooit/config.py
@@ -0,0 +1,21 @@
+# gruvbox theme
+
+black = "#282828"
+white = "#ebdbb2"
+grey = "#928374"
+red = "#cc241d"
+yellow = "#fabd2f"
+yellow_dark = "#d79921"
+
+BACKGROUND = black
+BAR_BACKGROUND = black
+WORKSPACES_BACKGROUND = black
+TODOS_BACKGROUND = black
+BORDER_DIM = grey + " 50%"
+BORDER_LIT = grey
+
+BORDER_TITLE_DIM = white, yellow_dark
+BORDER_TITLE_LIT = grey, yellow
+
+SEARCH_COLOR = red
+
